c955d6c2a527db493698f1724e7ac8310c556b6d
salvatore <salvatore.sorrentino@live.com>
2023-03-30 14:08:40 +0200

    correzione webview.js

--- DemoWPF/MainWindow.xaml.cs
+++ DemoWPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,9 @@ namespace DemoWPF
         public MainWindow()
         {
             InitializeComponent();
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
         }
     }
 }
