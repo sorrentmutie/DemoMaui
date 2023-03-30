3e7b36779e82880630e28faea4b2eb577b735035
salvatore <salvatore.sorrentino@live.com>
2023-03-30 09:47:42 +0200

    INotifyPropertyChanged

---
+++ Helpers/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DemoMaui.Helpers
+{
+    public class ViewModelBase: INotifyPropertyChanged
+    {
+        public void SetProperty<T>(ref T member, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (object.Equals(member, value)) return;
+            member = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
